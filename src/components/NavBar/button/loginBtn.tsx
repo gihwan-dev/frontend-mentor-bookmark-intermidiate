c60da9949a2f3fb8787091fc3d0221925f857c5c
CHOI GIHOAWN <rlghks3004@gmail.com>
2023-06-27 21:23:17 +0900

    네비게이션 작업 완료

---
+++ src/components/NavBar/button/loginBtn.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./loginBtn.module.scss";
+
+const LoginBtn = () => {
+  return (
+    <button type={"button"} onClick={() => {}} className={styles.btn}>
+      LOGIN
+    </button>
+  );
+};
+
+export default LoginBtn;
